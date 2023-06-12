1f91ebf7a9d3df3b4f32459f10433ebc604892b0
lmtroper <larissatroper@outlook.com>
2023-06-12 19:38:18 -0400

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Larissa Troper on 2023-06-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/language_learning_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  language_learning_appApp.swift
+//  Shared
+//
+//  Created by Larissa Troper on 2023-06-12.
+//
+
+import SwiftUI
+
+@main
+struct language_learning_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
